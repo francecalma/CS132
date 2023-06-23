e28ff97935e6320c05fae71887b55b62357f620b
francecalma <113011006+francecalma@users.noreply.github.com>
2023-06-24 04:24:38 +0800

    Create pre2.js

---
+++ pre2.js
@@ -0,0 +1,42 @@
+document.write('<link rel="stylesheet" href="https://github.githubassets.com/assets/gist-embed-38d69729aad4.css">')
+document.write('<div id=\"gist122499413\" class=\"gist\">
+    <div class=\"gist-file\" translate=\"no\">
+      <div class=\"gist-data\">
+        <div class=\"js-gist-file-update-container js-task-list-container file-box\">
+  <div id=\"file-Fake_News_Preprocessing-ipynb\" class=\"file my-2\">
+    
+    <div itemprop=\"text\" class=\"Box-body p-0 blob-wrapper data type-jupyter-notebook  \">
+
+          <div class=\"render-wrapper \">
+    <div class=\"render-container is-render-pending js-render-target \"
+      
+      data-host=\"https://notebooks.githubusercontent.com\"
+      data-type=\"ipynb\">
+      <svg style=\"box-sizing: content-box; color: var(--color-icon-primary);\" width=\"64\" height=\"64\" viewBox=\"0 0 16 16\" fill=\"none\" data-view-component=\"true\" class=\"octospinner mx-auto anim-rotate\">
+  <circle cx=\"8\" cy=\"8\" r=\"7\" stroke=\"currentColor\" stroke-opacity=\"0.25\" stroke-width=\"2\" vector-effect=\"non-scaling-stroke\" />
+  <path d=\"M15 8a7.002 7.002 0 00-7-7\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" vector-effect=\"non-scaling-stroke\" />
+<\/svg>
+      <div class=\"render-viewer-error\">Sorry, something went wrong. <a href="pre2.js">Reload?<\/a><\/div>
+      <div class=\"render-viewer-fatal\">Sorry, we cannot display this file.<\/div>
+      <div class=\"render-viewer-invalid\">Sorry, this file is invalid so it cannot be displayed.<\/div>
+      
+          Viewer requires iframe.
+      <\/iframe>
+    <\/div>
+  <\/div>
+ 
+    <\/div>
+
+  <\/div>
+<\/div>
+
+      <\/div>
+      <div class=\"gist-meta\">
+         <a href="Fake_News_Preprocessing.ipynb">
+          Fake_News_Preprocessing.ipynb
+        <\/a>
+        hosted with &#10084; by <a href=\"https://github.com\">GitHub<\/a>
+      <\/div>
+    <\/div>
+<\/div>
+')
